ca57d36b8fb42c88c9c1a6826c1490a9114de8ad
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2019-06-24 19:27:51 +0100

    rename some methods and variables

--- ejercicio_10.py
+++ ejercicio_10.py
@@ -12,21 +12,21 @@ FINAL_WORK = 15
 
 def final_qualification_by_criteria_of_qualification_in_matter_of_algorithms():
     try:
-        average_partial_1 = float(input('Enter qualification of first partial:\n'))
-        average_partial_2 = float(input('Enter qualification of second partial:\n'))
-        average_partial_3 = float(input('Enter qualification of third partial:\n'))
-            
+        partial_1 = float(input('Enter qualification of first partial:\n'))
+        partial_2 = float(input('Enter qualification of second partial:\n'))
+        partial_3 = float(input('Enter qualification of third partial:\n'))
+
         final_exam_qualification = float(input('Enter qualification of final exam:\n'))
         final_work_qualification = float(input('Enter qualification of final work:\n'))
     except:
         print 'Qualification must be a number'
 
-    if (__qualification_is_correct(average_partial_1)
-    and __qualification_is_correct(average_partial_2)
-    and __qualification_is_correct(average_partial_3)
-    and __qualification_is_correct(final_exam_qualification) 
-    and __qualification_is_correct(final_work_qualification)):
-        partials = [average_partial_1, average_partial_2, average_partial_3]
+    if (__qualification_in_range(partial_1)
+    and __qualification_in_range(partial_2)
+    and __qualification_in_range(partial_3)
+    and __qualification_in_range(final_exam_qualification) 
+    and __qualification_in_range(final_work_qualification)):
+        partials = [partial_1, partial_2, partial_3]
         
         total_qualification = __get_qualification_percent(__average(partials), AVERAGE_PARTIALS)        
         total_qualification += __get_qualification_percent(final_exam_qualification, FINAL_EXAM)
@@ -35,7 +35,7 @@ def final_qualification_by_criteria_of_qualification_in_matter_of_algorithms():
     else:
         print 'Qualification must be between 0 and 10'
 
-def __qualification_is_correct(qualification):
+def __qualification_in_range(qualification):
     if qualification >= 0 and qualification <= 10:
         return True
     else:
